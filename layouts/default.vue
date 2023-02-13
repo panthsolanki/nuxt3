f00544e2c415f464c6eaf5238df1cdd17b4e9a65
Panth <panth.solanki@gmail.com>
2023-02-13 14:46:57 +0530

    layout added

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <header class="shadow-sm bg-white">
+      <nav class="container mx-auto p-4 flex justify-between">
+        <NuxtLink to="/" class="font-bold">Site Name</NuxtLink>
+        <ul class="flex gap-4">
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="about">About</NuxtLink></li>
+          <li><NuxtLink to="profile">Profile</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+    <div class="container mx-auto p-4">
+      <slot></slot>
+    </div>
+  </div>
+</template>
